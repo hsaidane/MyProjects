abd16fb6b0cd675d2a1e08a4f564ba79efc30bf4
Hatem Saidane <hatem_saidane@yahoo.com>
2016-12-09 02:25:59 -0800

    TryPhpCds/index.php

--- TryPhpCds/index.php
+++ TryPhpCds/index.php
@@ -1,5 +1,5 @@
 <?php
-$data = file_get_contents('https://www.codeschool.com/users/110833.json');
+$data = file_get_contents('https://www.codeschool.com/users/hatemksar.json');
 $json_data = json_decode($data, true);
 // var_dump($json_data['courses']['completed']);
 $courses = $json_data['courses']['completed'];
